99d0b9ecdea53ff0342832cffbfc89b4b6fa0746
AshankDsouza <ajdsouz1@asu.edu>
2025-10-04 14:43:07 -0700

    allows containers to access each other

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Bind to all interfaces for Docker
     port: 3000, // Change this to your desired port number
     open: false, // Disabled for Docker compatibility
   },
